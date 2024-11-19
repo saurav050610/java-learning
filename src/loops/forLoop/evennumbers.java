d376dac16abddb0d30b7adab5f581585ba1961ff
Saurav Singh <sauravkumar050610@gmail.com>
2024-11-19 14:45:35 +0530

    even number

---
+++ src/loops/forLoop/evennumbers.java
@@ -0,0 +1,9 @@
+package loops.forLoop;
+
+public class evennumbers {
+    public static void main(String[] args){
+        for(int i=2;i<=10;i=i+2){
+            System.out.println("first 10 even numbers are" +i);
+        }
+    }
+}
