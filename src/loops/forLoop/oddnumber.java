26975d14679768e51e89b9f279f09163b73895cc
Saurav Singh <sauravkumar050610@gmail.com>
2024-11-19 14:41:31 +0530

    odd numbers

---
+++ src/loops/forLoop/oddnumber.java
@@ -0,0 +1,9 @@
+package loops.forLoop;
+
+public class oddnumber {
+    public static void main(String [] args){
+        for(int i=1;i<=10;i=i+2){
+            System.out.println("first ten odd numbers are" +i);
+        }
+    }
+}
